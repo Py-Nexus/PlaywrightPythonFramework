63e3d690d4b0e435b2c5f3b1198c5772204b3af8
amit-automationQA <kundaramit@gmail.com>
2025-07-31 22:01:20 +0530

    Enhanced pytest.ini and enabled logging for reportportal

--- testcases/Test_LoginPage.py
+++ testcases/Test_LoginPage.py
@@ -1,10 +1,11 @@
 from pages.LoginPage import LoginPage
+from testcases.conftest import rp_logger
 
 
-class Test_LoginPage():
+class Test_LoginPage:
 
-
-    def test_goto_test_login_page(self, page):
-        print("I'm in Test_LoginPage")
+    def test_goto_test_login_page(self, page, rp_logger):
         login = LoginPage(page)
+        rp_logger.info("Login Test Case Started")
+        #page.pause()    # The below pause is used to pause the test execution in Playwright Inspector
         login.navigate_to_test_login_page()
